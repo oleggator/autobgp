06e9ba6ffb3b683a55aecc1cdec5a17e6bae5b24
Oleg Utkin <o.utkin@corp.mail.ru>
2023-02-28 12:37:53 +0300

    make all-in-one app

--- main.go
+++ main.go
@@ -2,103 +2,158 @@ package main
 
 import (
 	"context"
-	"github.com/coredns/coredns/plugin"
-	"github.com/dnstap/golang-dnstap"
 	"github.com/miekg/dns"
 	api "github.com/osrg/gobgp/v3/api"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"github.com/osrg/gobgp/v3/pkg/server"
 	apb "google.golang.org/protobuf/types/known/anypb"
 	"log"
 )
 
-var dnstapListen = "127.0.0.1:6000"
-var gobgpGRPCAddr = "127.0.0.1:50051"
-var nextHop = "192.168.1.1"
-var zones = plugin.Zones{
-	"netflix.com.",
-}
+var (
+	nextHop = "192.168.1.1"
+
+	zones = Zones{
+		"netflix.com.",
+	}
+
+	asn       uint32 = 65432
+	routerID         = "192.168.88.247"
+	bgpListen int32  = 179
+
+	neighborAddress        = "192.168.88.1"
+	neighborAsn     uint32 = 64512
+
+	authoritativeDNS = "1.1.1.1:853"
+)
 
 func init() {
 	zones.Normalize()
 }
 
 func main() {
-	conn, err := grpc.DialContext(context.TODO(), gobgpGRPCAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	s := server.NewBgpServer()
+	go s.Serve()
+
+	if err := s.StartBgp(context.Background(), &api.StartBgpRequest{
+		Global: &api.Global{
+			Asn:        asn,
+			RouterId:   routerID,
+			ListenPort: bgpListen,
+		},
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	r := &api.WatchEventRequest{Peer: &api.WatchEventRequest_Peer{}}
+	if err := s.WatchEvent(context.Background(), r, func(r *api.WatchEventResponse) {
+		if p := r.GetPeer(); p != nil && p.Type == api.WatchEventResponse_PeerEvent_STATE {
+			log.Println(p)
+		}
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := s.AddPeer(context.Background(), &api.AddPeerRequest{
+		Peer: &api.Peer{
+			Conf: &api.PeerConf{
+				NeighborAddress: neighborAddress,
+				PeerAsn:         neighborAsn,
+			},
+		},
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	autoBGP, err := NewAutoBGP(s, nextHop)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	s := NewGoBGPAPI(conn)
 
-	dnstapHandler := func(message *dnstap.Message) {
-		if *message.Type != dnstap.Message_CLIENT_RESPONSE {
-			return
-		}
-
-		var msg dns.Msg
-		if err := msg.Unpack(message.ResponseMessage); err != nil {
-			log.Fatalln(err)
-		}
-
-		for _, answer := range msg.Answer {
-			answer, ok := answer.(*dns.A)
-			if !ok {
-				continue
-			}
-
-			if match := zones.Matches(answer.Header().Name); match == "" {
-				continue
-			}
-
-			ip := api.IPAddressPrefix{
-				Prefix:    answer.A.String(),
-				PrefixLen: 32,
-			}
-			if err := s.AddRoute(context.Background(), &ip, nextHop); err != nil {
-				log.Fatalln(err)
-			}
-		}
-	}
-	if err := listen(dnstapListen, dnstapHandler); err != nil {
+	if err := dns.ListenAndServe("0.0.0.0:53", "udp", autoBGP); err != nil {
 		log.Fatalln(err)
 	}
 }
 
-type GoBGPAPI struct {
-	s api.GobgpApiClient
+type AutoBGP struct {
+	dnsClient dns.Client
+	dnsConn   *dns.Conn
+
+	bgpServer *server.BgpServer
+
+	netxHopAttr *apb.Any
+	originAttr  *apb.Any
 }
 
-func NewGoBGPAPI(conn grpc.ClientConnInterface) GoBGPAPI {
-	return GoBGPAPI{s: api.NewGobgpApiClient(conn)}
-}
-
-func (a *GoBGPAPI) AddRoute(ctx context.Context, ip *api.IPAddressPrefix, nextHop string) error {
-	originAttr, err := apb.New(&api.OriginAttribute{Origin: 0})
-	if err != nil {
-		return err
+func NewAutoBGP(bgpServer *server.BgpServer, nextHop string) (*AutoBGP, error) {
+	autoBGP := &AutoBGP{
+		dnsClient: dns.Client{Net: "tcp-tls"},
+		bgpServer: bgpServer,
 	}
 
 	netxHopAttr, err := apb.New(&api.NextHopAttribute{NextHop: nextHop})
 	if err != nil {
-		return err
+		return nil, err
 	}
+	autoBGP.netxHopAttr = netxHopAttr
 
-	nlri, err := apb.New(ip)
+	conn, err := autoBGP.dnsClient.DialContext(context.Background(), authoritativeDNS)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	autoBGP.dnsConn = conn
 
-	_, err = a.s.AddPath(ctx, &api.AddPathRequest{
-		TableType: api.TableType_ADJ_OUT,
-		Path: &api.Path{
-			Nlri:   nlri,
-			Pattrs: []*apb.Any{originAttr, netxHopAttr},
-			Family: &api.Family{Afi: api.Family_AFI_IP, Safi: api.Family_SAFI_UNICAST},
-		},
-	})
+	originAttr, err := apb.New(&api.OriginAttribute{Origin: 0})
 	if err != nil {
-		return err
+		return nil, err
+	}
+	autoBGP.originAttr = originAttr
+
+	return autoBGP, nil
+}
+
+func (a *AutoBGP) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
+	defer w.Close()
+
+	resp, _, err := a.dnsClient.ExchangeWithConn(req, a.dnsConn)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	return nil
+	for _, answer := range resp.Answer {
+		answer, ok := answer.(*dns.A)
+		if !ok {
+			continue
+		}
+
+		if match := zones.Matches(answer.Header().Name); match == "" {
+			continue
+		}
+
+		ip := api.IPAddressPrefix{
+			Prefix:    answer.A.String(),
+			PrefixLen: 32,
+		}
+		log.Println(answer.Header().Name, ip.Prefix)
+
+		nlri, err := apb.New(&ip)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		_, err = a.bgpServer.AddPath(context.Background(), &api.AddPathRequest{
+			TableType: api.TableType_ADJ_OUT,
+			Path: &api.Path{
+				Nlri:   nlri,
+				Pattrs: []*apb.Any{a.originAttr, a.netxHopAttr},
+				Family: &api.Family{Afi: api.Family_AFI_IP, Safi: api.Family_SAFI_UNICAST},
+			},
+		})
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if err := w.WriteMsg(resp); err != nil {
+		log.Fatalln(err)
+	}
 }
---
+++ normalize.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/miekg/dns"
+	"strings"
+)
+
+// See core/dnsserver/address.go - we should unify these two impls.
+
+// Zones represents a lists of zone names.
+type Zones []string
+
+// Matches checks if qname is a subdomain of any of the zones in z.  The match
+// will return the most specific zones that matches. The empty string
+// signals a not found condition.
+func (z Zones) Matches(qname string) string {
+	zone := ""
+	for _, zname := range z {
+		if dns.IsSubDomain(zname, qname) {
+			// We want the *longest* matching zone, otherwise we may end up in a parent
+			if len(zname) > len(zone) {
+				zone = zname
+			}
+		}
+	}
+	return zone
+}
+
+// Normalize fully qualifies all zones in z. The zones in Z must be domain names, without
+// a port or protocol prefix.
+func (z Zones) Normalize() {
+	for i := range z {
+		z[i] = Name(z[i]).Normalize()
+	}
+}
+
+// Name represents a domain name.
+type Name string
+
+// Matches checks to see if other is a subdomain (or the same domain) of n.
+// This method assures that names can be easily and consistently matched.
+func (n Name) Matches(child string) bool {
+	if dns.Name(n) == dns.Name(child) {
+		return true
+	}
+	return dns.IsSubDomain(string(n), child)
+}
+
+// Normalize lowercases and makes n fully qualified.
+func (n Name) Normalize() string { return strings.ToLower(dns.Fqdn(string(n))) }
